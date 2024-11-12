9377cdd329f79c2720c493ec813d2ad757361ab1
Ivan Akira <i.akira@traveloka.com>
2024-11-12 20:51:55 +0700

    feat: solution

--- src/main/java/com/example/TicketMachine.java
+++ src/main/java/com/example/TicketMachine.java
@@ -3,6 +3,12 @@ package com.example;
 public class TicketMachine {
 
     public String getDetails(Ticket ticket) {
+        if (ticket instanceof AdultTicket(var price)) {
+            return STR."Adult ticket has price \{ price }";
+        }
+        if (ticket instanceof ChildTicket childTicket) {
+            return STR."Child ticket has price \{ childTicket.price() }";
+        }
         return "Unknown ticket type";
     }
 
